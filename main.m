e076e2c9e70c090ec5b327100d77d354e5fdb37a
Stephen Yang <stepheny755@gmail.com>
2020-02-02 12:40:43 -0500

    Update main.m

--- main.m
+++ main.m
@@ -31,30 +31,40 @@ slotGap = SLOT_PITCH-Bs1; %Width of an Individual Slot
 slotTeethWidth = (SLOTS-1)*SLOT_PITCH+slotGap;
 coilArea = slotGap*Hs2/2; %Area of coil for a single phase
 
-outputs = zeros(5,34);
-outputVoltage = zeros(3,34);
-outputCurrent = zeros(3,34);
-outputResistance = zeros(3,34);
+%Define simulations Number
+numSims = 1000;
+startVal = 5;
 
-for i=5:2000
-  inputCurrent = i*2
-  [losses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH)
-  outputs(1,i)=wstforcex;
-  outputs(2,i)=wstforcey;
-  outputs(3,i)=lforcex;
-  outputs(4,i)=lforcey
-  outputs(5,i)=losses;
-  outputVoltage(1,i)=vA;
-  outputVoltage(2,i)=vB;
-  outputVoltage(3,i)=vC;
-  outputCurrent(1,i)=cA;
-  outputCurrent(2,i)=cB;
-  outputCurrent(3,i)=cC;
-  outputResistance(1,i)=vA/cA;
-  outputResistance(2,i)=vB/cB;
-  outputResistance(3,i)=vC/cC;
+inputs = zeros(1,numSims);
+outputs = zeros(6,numSims);
+outputVoltage = zeros(3,numSims);
+outputCurrent = zeros(3,numSims);
+outputResistance = zeros(3,numSims);
+
+for index=startVal:numSims+startVal
+  inputCurrent = index*2;
+  i=index-startVal+1;
+  disp(inputCurrent);
+  disp(i);
+  [losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH);
+  inputs(1,i)=inputCurrent; %Input Current in amps
+  outputs(1,i)=wstforcex; %Weighted Stress Tensor Force on Track, x direction
+  outputs(2,i)=wstforcey; %Weighted Stress Tensor Force on Track, y direction
+  outputs(3,i)=lforcex; %Lorentz Force on Track, x direction
+  outputs(4,i)=lforcey; %Lorentz Force on Track, y direction
+  outputs(5,i)=losses; %Hysteresis Losses
+  outputs(6,i)=totalLosses; %Total Losses
+  outputVoltage(1,i)=vA; %Voltage of Phase A
+  outputVoltage(2,i)=vB; %Voltage of Phase B
+  outputVoltage(3,i)=vC; %Voltage of Phase C
+  outputCurrent(1,i)=cA; %Current of Phase A
+  outputCurrent(2,i)=cB; %Current of Phase B
+  outputCurrent(3,i)=cC; %Current of Phase C
+  outputResistance(1,i)=vA/cA; %Resistance of Phase A
+  outputResistance(2,i)=vB/cB; %Resistance of Phase B
+  outputResistance(3,i)=vC/cC; %Resistance of Phase C
 end
 
-
+save('params.mat','inputs','outputs','outputVoltage','outputCurrent','outputResistance');
 
 %DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH)
