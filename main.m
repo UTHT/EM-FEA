9f806eb33008ba7b82be9fca7f8feba8dae73168
Stephen Yang <stepheny755@gmail.com>
2020-02-05 17:00:57 -0500

    Update main.m
    added parameter descriptions

--- main.m
+++ main.m
@@ -1,20 +1,23 @@
 %Define LIM Parameters (Parallel to ANSYS rmXprt LinearMCore definition)
-WIDTH_CORE = 460;
-THICK_CORE = 50;
-LENGTH = 50;
-GAP = 17.5;
-SLOT_PITCH = 40;
-SLOTS = 11;
-Hs0 = 0;
-Hs01 = 0;
-Hs1 = 0;
-Hs2 = 20;
-Bs0 = 20;
-Bs1 = 20;
-Bs2 = 20;
-Rs = 0;
-Layers = 2;
-COIL_PITCH = 2;
+WIDTH_CORE = 460; %Core width in motion direction
+THICK_CORE = 50; %Core thickness
+LENGTH = 50; %Core length
+GAP = 17.5; %Gap between core and xy plane (or 1/2 of air gap)
+SLOT_PITCH = 40; %Distance between two slots
+SLOTS = 11; %Number of slots
+Hs0 = 0; %Slot opening height
+Hs01 = 0; %Slot closed bridge height
+Hs1 = 0; %Slot wedge height
+Hs2 = 20; %Slot body height
+Bs0 = 20; %Slot opening width
+Bs1 = 20; %Slot wedge maximum Width
+Bs2 = 20; %Slot body bottom width
+Rs = 0; %Slot body bottom fillet
+Layers = 2; %Number of winding layers
+COIL_PITCH = 2; %Coil Pitch measured in slots
+END_EXT = 0; %One-sided winding end extended length
+SPAN_EXT = 20; %Axial length of winding end span
+SEG_ANGLE = 15; %Deviation angle for slot arches
 
 %Define Simulation Default Parameters
 inputCurrent = 10;
