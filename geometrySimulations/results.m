ff07caf1c1de60065434df4bd06b3a05b61efa72
Stephen Yang <stepheny755@gmail.com>
2020-02-07 14:28:54 -0500

    Update results.m
    created results viewer matlab code

--- geometrySimulations/results.m
+++ geometrySimulations/results.m
@@ -1,2 +1,29 @@
-load('geometry_results.mat');
+load('geometry_results_0205.mat');
+
+figure(1);
 surf(outputResultX);
+title("Force/Weight Ratio");
+xlabel('Depth (Hs2)');
+ylabel('Width (Bs2)');
+zlabel('Force/Weight');
+
+figure(4);
+surf(outputLForcex);
+title("Force");
+xlabel('Depth (Hs2)');
+ylabel('Width (Bs2)');
+zlabel('Force');
+
+figure(2);
+surf(inputVolume);
+title("Volume of Both Cores");
+xlabel('Depth (Hs2)');
+ylabel('Width (Bs2)');
+zlabel('Volume');
+
+figure(3);
+surf(outputHLosses);
+title("Core Losses");
+xlabel('Depth (Hs2)');
+ylabel('Width (Hs2)');
+zlabel('Core Losse');
