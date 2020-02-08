63fbbb182cfdf365efa2b512568152a863c7dc94
Stephen Yang <stepheny755@gmail.com>
2020-02-08 15:09:30 -0500

    updated result plotter and files

--- geometrySimulations/results.m
+++ geometrySimulations/results.m
@@ -1,29 +1,45 @@
-load('geometry_results_0205.mat');
+load('geometry_results.mat');
 
 figure(1);
 surf(outputResultX);
 title("Force/Weight Ratio");
-xlabel('Depth (Hs2)');
-ylabel('Width (Bs2)');
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
 zlabel('Force/Weight');
 
 figure(4);
 surf(outputLForcex);
 title("Force");
-xlabel('Depth (Hs2)');
-ylabel('Width (Bs2)');
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
 zlabel('Force');
 
 figure(2);
 surf(inputVolume);
 title("Volume of Both Cores");
-xlabel('Depth (Hs2)');
-ylabel('Width (Bs2)');
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
 zlabel('Volume');
 
 figure(3);
 surf(outputHLosses);
 title("Core Losses");
-xlabel('Depth (Hs2)');
-ylabel('Width (Hs2)');
-zlabel('Core Losse');
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
+zlabel('Core Losses');
+
+
+figure(4);
+surf(inputCoilArea);
+title("Coil Area");
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
+zlabel('Coil Area');
+
+
+figure(5);
+surf(outputLForcex./inputCoilArea);
+title("Force/Coil Area");
+xlabel('Width (Bs2)');
+ylabel('Depth (Hs2)');
+zlabel('Force/Coil Area');
