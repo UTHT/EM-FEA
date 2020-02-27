526b3e5e68cd3538f893dfe8ad9ffcd3e99f2171
Stephen Yang <stepheny755@gmail.com>
2020-02-27 15:39:52 -0500

    Update main.m
    updated value of coil turns

--- AirGap/main.m
+++ AirGap/main.m
@@ -26,7 +26,7 @@ SEG_ANGLE = 15; %Deviation angle for slot arches
 %Define Simulation Default Parameters
 inputCurrent = 10;
 freq = 15;
-coilTurns = 360;
+coilTurns = 310;
 trackThickness = 8;
 copperMaterial = '20 AWG';
 trackMaterial = 'Aluminum, 6061-T6';
@@ -47,7 +47,7 @@ slotTeethWidth = (SLOTS-1)*SLOT_PITCH+slotGap;
 coilArea = slotGap*Hs2/2; %Area of coil for a single phase
 
 %Define simulations bounds
-min_gap = 4;
+min_gap = 5;
 max_gap = 30;
 
 %Simulation counter/duration Variables
@@ -55,56 +55,53 @@ totalTimeElapsed = 0;
 singleSimTimeElapsed = 0;
 simulationNumber = 1;
 
-%outputWSTForcex=imresize(outputWSTForcex,[max_thickness,max_width]  )
+parfor x=min_gap*10:max_gap*10
 
-% parfor x=min_gap*10:max_gap*10
-% 
-%   simulationNumber=x-min_gap*10;
-%   tic
-%   gap = x/10;
-%   WIDTH_CORE = Bs2*(SLOTS)+20*(SLOTS-1)+2*END_EXT;
-%   Volume = THICK_CORE/10*WIDTH_CORE/10*LENGTH/10-Bs2/10*Hs2/10*LENGTH/10*(SLOTS); %Volume of DLIM in cm3
-%   Weight = Volume*coreMaterialDensity*2; %Weight of DLIM Core in g
-% 
-%   inputHs2(x)=Hs2;
-%   inputDepth(x)=THICK_CORE;
-%   inputBs2(x)=Bs2;
-%   inputWidth(x)=WIDTH_CORE;
-%   inputEndExt(x)=END_EXT;
-%   inputNi(x)=inputCurrent*coilTurns; %Ni
-%   inputWeight(x)=Weight; %Weight of a single core (one side)
-%   inputVolume(x)=Volume;
-%   inputCoilArea(x)=coilArea;
-% 
-%   [losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC,flA,flB,flC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,simulationNumber);
-%   outputWSTForcex(x)=wstforcex; %Weighted Stress Tensor Force on Track, x direction
-%   outputWSTForcey(x)=wstforcey; %Weighted Stress Tensor Force on Track, y direction
-%   outputLForcex(x)=lforcex; %Lorentz Force on Track, x direction
-%   outputLForcey(x)=lforcey; %Lorentz Force on Track, y direction
-%   outputHLosses(x)=losses; %Hysteresis Losses
-%   outputTLosses(x)=totalLosses; %Total Losses
-%   outputVoltageA(x)=vA; %Voltage of Phase A
-%   outputVoltageB(x)=vB; %Voltage of Phase B
-%   outputVoltageC(x)=vC; %Voltage of Phase C
-%   outputCurrentA(x)=cA; %Current of Phase A
-%   outputCurrentB(x)=cB; %Current of Phase B
-%   outputCurrentC(x)=cC; %Current of Phase C
-%   outputResistanceA(x)=vA/cA; %Resistance of Phase A
-%   outputResistanceB(x)=vB/cB; %Resistance of Phase B
-%   outputResistanceC(x)=vC/cC; %Resistance of Phase C
-%   outputFluxLinkageA(x)=flA;
-%   outputFluxLinkageB(x)=flB;
-%   outputFluxLinkageC(x)=flC;
-%   outputResultX(x)=lforcex/Weight;%Force/Weight Ratio (x-direction)
-%   outputResultY(x)=lforcey/Weight;%Force/Weight Ratio (y-direction)
-% 
-%   singleSimTimeElapsed=toc;
-%   disp(append("Simulation Number ",num2str(simulationNumber)," completed in ",num2str(singleSimTimeElapsed)," seconds with parameters THICK_CORE=",num2str(THICK_CORE),"WIDTH_CORE=",num2str(WIDTH_CORE)))
-% 
-%   totalTimeElapsed = totalTimeElapsed+singleSimTimeElapsed;
-% end
+  simulationNumber=x-min_gap*10;
+  tic
+  GAP = x/10;
+  disp(append("Starting simulation number ",num2str(simulationNumber)," with parameters GAP=",sprintf("%0.2f",GAP)))
+  WIDTH_CORE = Bs2*(SLOTS)+20*(SLOTS-1)+2*END_EXT;
+  Volume = THICK_CORE/10*WIDTH_CORE/10*LENGTH/10-Bs2/10*Hs2/10*LENGTH/10*(SLOTS); %Volume of DLIM in cm3
+  Weight = Volume*coreMaterialDensity*2; %Weight of DLIM Core in g
 
-[losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC,flA,flB,flC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,simulationNumber);
+  inputHs2(x)=Hs2;
+  inputDepth(x)=THICK_CORE;
+  inputBs2(x)=Bs2;
+  inputWidth(x)=WIDTH_CORE;
+  inputEndExt(x)=END_EXT;
+  inputNi(x)=inputCurrent*coilTurns; %Ni
+  inputWeight(x)=Weight; %Weight of a single core (one side)
+  inputVolume(x)=Volume;
+  inputCoilArea(x)=coilArea;
+
+  [losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC,flA,flB,flC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,END_EXT,simulationNumber);
+  outputWSTForcex(x)=wstforcex; %Weighted Stress Tensor Force on Track, x direction
+  outputWSTForcey(x)=wstforcey; %Weighted Stress Tensor Force on Track, y direction
+  outputLForcex(x)=lforcex; %Lorentz Force on Track, x direction
+  outputLForcey(x)=lforcey; %Lorentz Force on Track, y direction
+  outputHLosses(x)=losses; %Hysteresis Losses
+  outputTLosses(x)=totalLosses; %Total Losses
+  outputVoltageA(x)=vA; %Voltage of Phase A
+  outputVoltageB(x)=vB; %Voltage of Phase B
+  outputVoltageC(x)=vC; %Voltage of Phase C
+  outputCurrentA(x)=cA; %Current of Phase A
+  outputCurrentB(x)=cB; %Current of Phase B
+  outputCurrentC(x)=cC; %Current of Phase C
+  outputResistanceA(x)=vA/cA; %Resistance of Phase A
+  outputResistanceB(x)=vB/cB; %Resistance of Phase B
+  outputResistanceC(x)=vC/cC; %Resistance of Phase C
+  outputFluxLinkageA(x)=flA;
+  outputFluxLinkageB(x)=flB;
+  outputFluxLinkageC(x)=flC;
+  outputResultX(x)=lforcex/Weight;%Force/Weight Ratio (x-direction)
+  outputResultY(x)=lforcey/Weight;%Force/Weight Ratio (y-direction)
+
+  singleSimTimeElapsed=toc;
+  disp(append("Simulation number ",num2str(simulationNumber)," completed in ",num2str(singleSimTimeElapsed)," seconds with parameters GAP=",num2str(GAP)))
+
+  totalTimeElapsed = totalTimeElapsed+singleSimTimeElapsed;
+end
 
 
 disp(append("Total Simulation Time: ",num2str(totalTimeElapsed),"seconds"))
