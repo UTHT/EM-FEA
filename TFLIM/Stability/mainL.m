e0b824b526d6e3f3c9cb0c7a8dd124547ae6e1b5
Stephen Yang <stepheny755@gmail.com>
2020-04-07 12:53:53 -0700

    updated simulation results

--- TFLIM/Stability/mainL.m
+++ TFLIM/Stability/mainL.m
@@ -37,7 +37,7 @@ save('stability_results.mat');
 
 parfor simulationNumber = 1:simulationMax
   tic;
-  for angleIndex = 1:angleDivisions;
+  for angleIndex = 1:angleDivisions
     trackoffset = simulationNumber;
     angle=(angleIndex-1)*angleIncrement;
     [losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vL,vR,cL,cR,flL,flR] = TFLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH,THICK_CORE,LENGTH,TEETH_THICKNESS,CORE_THICKNESS,COIL_WIDTH,TEETH_EXTENSIONS,AIR_GAP,TRACK_OFFSET,angle,simulationNumber);
