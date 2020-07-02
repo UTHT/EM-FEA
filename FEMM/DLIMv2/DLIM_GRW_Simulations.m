eb63c87d4caa228a944a6fdd263ab48134a7b9a9
Stephen Yang <stepheny755@gmail.com>
2020-07-02 14:00:16 -0700

    renamed GRW simulation files
    testing new gramme ring winding arrangement

--- FEMM/DLIMv2/DLIM_GRW_Simulations.m
+++ FEMM/DLIMv2/DLIM_GRW_Simulations.m
@@ -1,4 +1,4 @@
-function [hysteresisLosses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,phaseAvol,phaseBvol,phaseCvol,phaseAcur,phaseBcur,phaseCcur,phaseAfl,phaseBfl,phaseCfl] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,END_EXT)
+function [hysteresisLosses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,phaseAvol,phaseBvol,phaseCvol,phaseAcur,phaseBcur,phaseCcur,phaseAfl,phaseBfl,phaseCfl] = DLIM_GRW_Simulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,CORE_ENDLENGTH)
 
 %Define simulation/modeller units
 unit = 'millimeters';
@@ -12,7 +12,6 @@ sumSlotTeeth = SLOTS*2+1; %Number of Teeth + Slots
 slotGap = SLOT_PITCH-Bs1; %Width of an Individual Slot
 slotTeethWidth = (SLOTS-1)*SLOT_PITCH+slotGap;
 coilArea = slotGap*Hs2/2; %Area of coil for a single phase
-END_EXT=END_EXT+slotGap;
 
 %Open FEMM and resize window
 openfemm(0);
@@ -42,14 +41,14 @@ slotTeethWidth = (SLOTS-1)*SLOT_PITCH+slotGap;
 %Start define LIM Core Geometry
 
 %Define LIM core external dimensions
-mi_addnode(WIDTH_CORE/-2-END_EXT,THICK_CORE);
-mi_addnode(WIDTH_CORE/2+END_EXT,THICK_CORE);
-mi_addnode(WIDTH_CORE/-2-END_EXT,0);
-mi_addnode(WIDTH_CORE/2+END_EXT,0);
+mi_addnode(WIDTH_CORE/-2-CORE_ENDLENGTH,THICK_CORE);
+mi_addnode(WIDTH_CORE/2+CORE_ENDLENGTH,THICK_CORE);
+mi_addnode(WIDTH_CORE/-2-CORE_ENDLENGTH,0);
+mi_addnode(WIDTH_CORE/2+CORE_ENDLENGTH,0);
 
-mi_addsegment(WIDTH_CORE/-2-END_EXT,THICK_CORE,WIDTH_CORE/2+END_EXT,THICK_CORE);
-mi_addsegment(WIDTH_CORE/-2-END_EXT,0,WIDTH_CORE/-2-END_EXT,THICK_CORE);
-mi_addsegment(WIDTH_CORE/2+END_EXT,0,WIDTH_CORE/2+END_EXT,THICK_CORE);
+mi_addsegment(WIDTH_CORE/-2-CORE_ENDLENGTH,THICK_CORE,WIDTH_CORE/2+CORE_ENDLENGTH,THICK_CORE);
+mi_addsegment(WIDTH_CORE/-2-CORE_ENDLENGTH,0,WIDTH_CORE/-2-CORE_ENDLENGTH,THICK_CORE);
+mi_addsegment(WIDTH_CORE/2+CORE_ENDLENGTH,0,WIDTH_CORE/2+CORE_ENDLENGTH,THICK_CORE);
 
 %Define teeth geometries
 for i=0:SLOTS-1
@@ -64,8 +63,8 @@ end
 %    mi_addsegment(-slotTeethWidth/2+slotGap+delta,0,-slotTeethWidth/2+slotGap+delta+Bs1,0);
 %end
 
-mi_addsegment(WIDTH_CORE/-2-END_EXT,0,-slotTeethWidth/2,0);
-mi_addsegment(slotTeethWidth/2,0,WIDTH_CORE/2+END_EXT,0);
+mi_addsegment(WIDTH_CORE/-2-CORE_ENDLENGTH,0,-slotTeethWidth/2,0);
+mi_addsegment(slotTeethWidth/2,0,WIDTH_CORE/2+CORE_ENDLENGTH,0);
 
 %Define Coil Currents
 wt = 0*2*pi*freq;%*time;
@@ -133,7 +132,7 @@ mi_selectlabel(slotTeethWidth/2-slotGap/2-SLOT_PITCH,Hs2*1/4);
 mi_setblockprop(Air,1,0,'<None>',0,0,0);
 
 %Select LIM Geometry and set to group 1
-mi_selectrectangle(WIDTH_CORE/-2-END_EXT,0,WIDTH_CORE/2+END_EXT,THICK_CORE,4);
+mi_selectrectangle(WIDTH_CORE/-2-CORE_ENDLENGTH,0,WIDTH_CORE/2+CORE_ENDLENGTH,THICK_CORE,4);
 mi_setgroup(1);
 
 %Label LIM Geometry
@@ -146,8 +145,8 @@ mi_selectgroup(1);
 mi_movetranslate(0,GAP);
 
 %Create and Label Air Gap
-mi_addsegment(WIDTH_CORE/-2-END_EXT,GAP,WIDTH_CORE/2+END_EXT,GAP);
-mi_selectrectangle(WIDTH_CORE/-2-END_EXT,trackThickness/2,WIDTH_CORE/2+END_EXT,GAP);
+mi_addsegment(WIDTH_CORE/-2-CORE_ENDLENGTH,GAP,WIDTH_CORE/2+CORE_ENDLENGTH,GAP);
+mi_selectrectangle(WIDTH_CORE/-2-CORE_ENDLENGTH,trackThickness/2,WIDTH_CORE/2+CORE_ENDLENGTH,GAP);
 mi_setgroup(1);
 %mi_addblocklabel(0,GAP/2);
 %mi_selectlabel(0,GAP/2);
@@ -155,11 +154,11 @@ mi_setblockprop(Air,1,0,'<None>',0,1,0);
 
 %Mirror one side to create DLIM
 mi_selectgroup(1);
-mi_mirror(WIDTH_CORE/-2-END_EXT,0,WIDTH_CORE/2+END_EXT,0);
+mi_mirror(WIDTH_CORE/-2-CORE_ENDLENGTH,0,WIDTH_CORE/2+CORE_ENDLENGTH,0);
 
 %Create Track
-mi_drawrectangle(WIDTH_CORE/-2-END_EXT-100,trackThickness/-2,WIDTH_CORE/2+END_EXT+100,trackThickness/2);
-mi_selectrectangle(WIDTH_CORE/-2-END_EXT-100,trackThickness/-2,WIDTH_CORE/2+END_EXT+100,trackThickness/2);
+mi_drawrectangle(WIDTH_CORE/-2-CORE_ENDLENGTH-100,trackThickness/-2,WIDTH_CORE/2+CORE_ENDLENGTH+100,trackThickness/2);
+mi_selectrectangle(WIDTH_CORE/-2-CORE_ENDLENGTH-100,trackThickness/-2,WIDTH_CORE/2+CORE_ENDLENGTH+100,trackThickness/2);
 mi_setgroup(2);
 mi_addblocklabel(0,0);
 mi_selectlabel(0,0);
@@ -172,9 +171,9 @@ mi_addblocklabel(0,THICK_CORE*2);
 mi_selectlabel(0,THICK_CORE*2);
 mi_setblockprop(Air,1,0,'<None>',0,0,0);
 mi_makeABC;
-mi_saveas('SimulationData\DLIMSimulations.fem');
-mi_analyze;
-mi_loadsolution;
+mi_saveas('qnafiles\DLIM-GRWsim.fem');
+%mi_analyze;
+%mi_loadsolution;
 
 mo_selectblock(0,0);
 lforcex = mo_blockintegral(11);
@@ -207,8 +206,8 @@ phaseCfl = phaseCprop(3);
 mo_clearblock;
 
 %Save Image
-leftBound = (WIDTH_CORE/-2-END_EXT-100)*paddingRatio;
-rightBound = (WIDTH_CORE/2+END_EXT+100)*paddingRatio;
+leftBound = (WIDTH_CORE/-2-CORE_ENDLENGTH-100)*paddingRatio;
+rightBound = (WIDTH_CORE/2+CORE_ENDLENGTH+100)*paddingRatio;
 topBound = (100)*paddingRatio;
 botBound = -(150)*paddingRatio;
 mo_showdensityplot(0,0,plotUpperLimit,plotLowerLimit,'real');
--- FEMM/DLIMv2/main.m
+++ FEMM/DLIMv2/main.m
@@ -1,23 +1,31 @@
   %Define LIM Parameters (Parallel to ANSYS rmXprt LinearMCore definition)
-WIDTH_CORE = 370; %Core width in motion direction
-THICK_CORE = 60; %Core thickness
+WIDTH_CORE = 500; %Core width in motion direction
+THICK_CORE = 80; %Core thickness
 LENGTH = 50; %Core length
 GAP = 6; %Gap between core and xy plane (or 1/2 of air gap)
-SLOT_PITCH = 40; %Distance between two slots
-SLOTS = 11; %Number of slots
+SLOT_PITCH = 60; %Distance between two slots
+SLOTS = 12; %Number of slots
 Hs0 = 0; %Slot opening height
 Hs01 = 0; %Slot closed bridge height
 Hs1 = 0; %Slot wedge height
 Hs2 = 40; %Slot body height (Height of teeth gap)
 Bs0 = 20; %Slot opening width
 Bs1 = 20; %Slot wedge maximum Width
-Bs2 = 10; %Slot body bottom width
+Bs2 = 20; %Slot body bottom width
 Rs = 0; %Slot body bottom fillet
 Layers = 2; %Number of winding layers
 COIL_PITCH = 2; %Coil Pitch measured in slots
 END_EXT = 30; %One-sided winding end extended length
 SPAN_EXT = 20; %Axial length of winding end span
 SEG_ANGLE = 15; %Deviation angle for slot arches
+
+%LIM Parameter generation from slot_pitch
+teeth_thickness = 20;
+slot_thickness = 40;
+core_endlength = 30;
+SLOT_PITCH = teeth_thickness+slot_thickness;
+Bs2 = slot_thickness;
+WIDTH_CORE=SLOT_PITCH*(SLOTS-1)+slot_thickness;
 
 %Define Simulation Default Parameters
 inputCurrent = 10;
@@ -34,4 +42,4 @@ slotGap = SLOT_PITCH-Bs1; %Width of an Individual Slot
 slotTeethWidth = (SLOTS-1)*SLOT_PITCH+slotGap;
 coilArea = slotGap*Hs2/2; %Area of coil for a single phase
 
-[losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC,flA,flB,flC] = DLIMSimulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,END_EXT);
+[losses,totalLosses,lforcex,lforcey,wstforcex,wstforcey,vA,vB,vC,cA,cB,cC,flA,flB,flC] = DLIM_GRW_Simulations(inputCurrent,freq,coilTurns,trackThickness,copperMaterial,coreMaterial,trackMaterial,WIDTH_CORE,THICK_CORE,LENGTH,GAP,SLOT_PITCH,SLOTS,Hs0,Hs01,Hs1,Hs2,Bs0,Bs1,Bs2,Rs,Layers,COIL_PITCH,core_endlength);
