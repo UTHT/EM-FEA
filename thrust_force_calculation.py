cc3cf25f03e1fffba85ca8a73a4fef7aff87c730
pun49030 <73437143+pun49030@users.noreply.github.com>
2021-06-14 09:12:25 +0700

    Add files via upload

---
+++ thrust_force_calculation.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+from scipy import integrate
+
+
+'''
+Variables:
+N = number of turns
+I = current
+g = air gap
+mu = absolute permeability (mu_r = mu/mu_0)
+
+
+
+
+
+
+B1 = (3*np.sqrt(2)*N*I/(np.sqrt((np.pi*g/mu)**2)))
