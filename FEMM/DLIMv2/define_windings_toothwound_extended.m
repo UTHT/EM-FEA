5cb069c1460df882b1a2d36069e806855007798d
Stephen Yang <stepheny755@gmail.com>
2021-10-15 14:07:27 -0400

    added new winding config

---
+++ FEMM/DLIMv2/define_windings_toothwound_extended.m
@@ -0,0 +1,53 @@
+function define_windings_toothwound_extended(SLOTS,SLOT_PITCH,copperMaterial,mediumMaterial,coilTurns,slotGap,slotTeethWidth,Hs2)
+
+for i=0:SLOTS-1
+   delta=SLOT_PITCH*i;
+
+   Phase = mod(i,12);
+   modulo = 2*mod(i+1,2)-1;
+   bWinding = '';
+   tWinding = '';
+  
+   if(Phase < 4)
+       bWinding='WindingA';
+       tWinding='WindingA';
+   elseif(Phase < 8)
+       bWinding='WindingB';
+       tWinding='WindingB';
+   else
+       bWinding='WindingC';
+       tWinding='WindingC';
+   end
+
+   %disp(bPhase)
+
+    mi_drawline(-slotTeethWidth/2+delta+slotGap/2,0,-slotTeethWidth/2+delta+slotGap/2,Hs2);
+%    
+%     mi_addblocklabel(-slotTeethWidth/2+slotGap/2+delta,Hs2*3/4);
+%     mi_selectlabel(-slotTeethWidth/2+slotGap/2+delta,Hs2*3/4);
+%     mi_setblockprop(copperMaterial,1,0,tWinding,0,1,coilTurns*modulo);
+%     mi_clearselected;
+%     mi_addblocklabel(-slotTeethWidth/2+slotGap/2+delta,Hs2*1/4);
+%     mi_selectlabel(-slotTeethWidth/2+slotGap/2+delta,Hs2*1/4);
+%     mi_setblockprop(copperMaterial,1,0,bWinding,0,1,coilTurns*modulo);
+%     mi_clearselected;
+end
+
+% Create external wound coils
+mi_drawline(-slotTeethWidth/2-slotGap/2,0,-slotTeethWidth/2-slotGap/2,Hs2);
+mi_drawline(-slotTeethWidth/2-slotGap/2,0,-slotTeethWidth/2,0);
+mi_drawline(-slotTeethWidth/2-slotGap/2,Hs2,-slotTeethWidth/2-slotGap/2,Hs2);
+
+% 
+% mi_addblocklabel(-slotTeethWidth/2+slotGap/2,Hs2*3/4);
+% mi_selectlabel(-slotTeethWidth/2+slotGap/2,Hs2*3/4);
+% mi_addblocklabel(-slotTeethWidth/2+slotGap/2+SLOT_PITCH,Hs2*3/4);
+% mi_selectlabel(-slotTeethWidth/2+slotGap/2+SLOT_PITCH,Hs2*3/4);
+% 
+% mi_addblocklabel(slotTeethWidth/2-slotGap/2,Hs2*1/4);
+% mi_selectlabel(slotTeethWidth/2-slotGap/2,Hs2*1/4);
+% mi_addblocklabel(slotTeethWidth/2-slotGap/2-SLOT_PITCH,Hs2*1/4);
+% mi_selectlabel(slotTeethWidth/2-slotGap/2-SLOT_PITCH,Hs2*1/4);
+% 
+% mi_setblockprop(mediumMaterial,1,0,'<None>',0,0,0);
+mi_clearselected;
